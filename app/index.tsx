d9c9ee7300fd82baec3f6f34fa7c756269aaa289
vgsnv <vgsnv@yandex.ru>
2017-07-03 22:55:07 +0300

    Add cssreset

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as css from './main.less';
+import * as css from './css/main.less';
  
 ReactDOM.render(
   <div className={css.button}>"Hello"</div>,
